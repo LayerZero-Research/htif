d35fa14276f2fffd0a4c7cf15836a298c203e7df
Zou Guangxian <zouguangxian@gmail.com>
2025-12-08 10:34:26 +0000

    feat: implement DebugWriter and update print macros for HTIF console output

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,7 @@
 
 pub mod htif;
 mod macros;
+mod writer;
 
 pub use htif::*;
+pub use writer::DebugWriter;
--- src/macros.rs
+++ src/macros.rs
@@ -1,60 +1,14 @@
 // Print macros for HTIF console output
+
 #[macro_export]
 macro_rules! println {
-    // Empty println!()
-    () => {
-        $crate::putchar(b'\n');
-    };
-
-    // String literals - no allocation needed
-    ($lit:literal) => {
-        {
-            for byte in $lit.bytes() {
-                $crate::putchar(byte);
-            }
-            $crate::putchar(b'\n');
-        }
-    };
-
-    // Everything else - use format! just like std::println!
-    ($($arg:tt)*) => {
-        {
-            extern crate alloc;
-            let formatted = alloc::format!($($arg)*);
-            for byte in formatted.bytes() {
-                $crate::putchar(byte);
-            }
-            $crate::putchar(b'\n');
-        }
-    };
-}
+        ($($arg:tt)*) => {{
+            use core::fmt::Write;
+            let _ = core::writeln!($crate::DebugWriter, $($arg)*);
+        }};
+    }
 
 #[macro_export]
 macro_rules! eprintln {
-    // Empty eprintln!()
-    () => {
-        $crate::putchar(b'\n');
-    };
-
-    // String literals - no allocation needed
-    ($lit:literal) => {
-        {
-            for byte in $lit.bytes() {
-                $crate::putchar(byte);
-            }
-            $crate::putchar(b'\n');
-        }
-    };
-
-    // Everything else - use format! just like std::eprintln!
-    ($($arg:tt)*) => {
-        {
-            extern crate alloc;
-            let formatted = alloc::format!($($arg)*);
-            for byte in formatted.bytes() {
-                $crate::putchar(byte);
-            }
-            $crate::putchar(b'\n');
-        }
-    };
+    ($($arg:tt)*) => { $crate::println!($($arg)*); }
 }
